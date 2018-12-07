33ac05ec78d418d125d7c56704c1ec9ef1e00a02
Mark Morales <marktravismorales@gmail.com>
2018-12-07 14:19:42 -0600

    Passed data to different VCs

--- Swoosh/Model/Player.swift
+++ Swoosh/Model/Player.swift
@@ -5,10 +5,14 @@
 //  Created by Mark Morales on 12/7/18.
 //  Copyright © 2018 Mark Morales. All rights reserved.
 //
+// var myData: String! Implicitly unwrapped optional:
+// Use when you can guarantee variable will have data in it before used
+// var myData: String? Optional:
+// Use when there may or may not be data in the variable at runtime
 
 import Foundation
 
 struct Player {
-    var desiredLeague: String?
-    var selectedSkillLevel: String?
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
 }
