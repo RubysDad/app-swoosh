b7f41232a9b39bb4ad3131c47dc943acc18361e3
Mark Morales <marktravismorales@gmail.com>
2018-12-07 13:52:08 -0600

    Created Player model and IBActions for League VC buttons

---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Mark Morales on 12/7/18.
+//  Copyright © 2018 Mark Morales. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
